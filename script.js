ee45a4f8365b26d312060c793e7e16c2e3be83eb
E-ArisRifaldi-236 <101787826+E-ArisRifaldi-236@users.noreply.github.com>
2026-02-10 13:43:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// function pesanWA(layanan) {
+//     const nomorWA = "6282123012526"; // ganti jika perlu (tanpa +)
+//     const pesan = `Assalamu’alaikum, saya ingin bertanya dan konsultasi mengenai layanan *${layanan}* di Griya Sehat Holistik Naafilah.`;
+
+//     const url = `https://wa.me/${nomorWA}?text=${encodeURIComponent(pesan)}`;
+    
+//     window.open(url, "_blank");
+// }
+
+// function beliWA(beli) {
+//     const nomorWA = "6282123012526"; // ganti jika perlu (tanpa +)
+    
+//     const pesan = `Assalamu’alaikum, saya ingin beli *${beli}*.`;
+
+//     const url = `https://wa.me/${nomorWA}?text=${encodeURIComponent(pesan)}`;
+    
+//     window.open(url, "_blank");
+// }
+
+
+function pesanWA(layanan) {
+    const nomor1 = "6282123012526"; // nomor WA 1
+
+
+    const pesan = `Assalamu’alaikum, saya ingin konsultasi mengenai *${layanan}* di Griya Sehat Holistik Naafilah.`;
+
+    const url1 = `https://wa.me/${nomor1}?text=${encodeURIComponent(pesan)}`;
+
+    // Buka WA ke dua nomor
+    window.open(url1, "_blank");
+}
+
+function pesan2WA(layanan) {
+    const nomor2 = "6285169847445"; // nomor WA 2
+
+    const pesan = `Assalamu’alaikum, saya ingin konsultasi mengenai *${layanan}* di Griya Sehat Holistik Naafilah.`;
+
+    const url2 = `https://wa.me/${nomor2}?text=${encodeURIComponent(pesan)}`;
+
+    // Buka WA ke dua nomor
+    window.open(url2, "_blank");
+}
+
+
+
+function beliWA(layanan) {
+    const nomor1 = "6282123012526"; // nomor WA 1
+
+
+    const pesan = `Assalamu’alaikum, saya ingin beli *${layanan}*.`;
+
+    const url1 = `https://wa.me/${nomor1}?text=${encodeURIComponent(pesan)}`;
+
+    // Buka WA ke dua nomor
+    window.open(url1, "_blank");
+}
+
+function beli2WA(layanan) {
+    const nomor2 = "6285169847445"; // nomor WA 2
+
+    const pesan = `Assalamu’alaikum, saya ingin beli *${layanan}*.`;
+
+    const url2 = `https://wa.me/${nomor2}?text=${encodeURIComponent(pesan)}`;
+
+    // Buka WA ke dua nomor
+    window.open(url2, "_blank");
+}
